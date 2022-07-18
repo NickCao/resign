96088357d6a50a06de80865d76f901946b766273
Nick Cao <nickcao@nichi.co>
2022-07-18 08:54:33 +0800

    mimic gpg pinentry

--- src/main.rs
+++ src/main.rs
@@ -59,8 +59,8 @@ fn main() -> Result<()> {
             let mut input =
                 PassphraseInput::with_default_binary().ok_or(anyhow!("pinentry not found"))?;
             let pin = input
-                .with_description("enter pin for card")
-                .with_prompt("pin:")
+                .with_description("Please unlock the card")
+                .with_prompt("PIN")
                 .interact()
                 .map_err(|e| anyhow!(e.to_string()))?;
             open.verify_user_for_signing(pin.expose_secret().as_bytes())?;
