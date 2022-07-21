0a094caa7f54be38b5426cf8a226ce3a9ac4acfa
Nick Cao <nickcao@nichi.co>
2022-07-21 11:07:44 +0800

    impl sequoia rpc

--- src/bin/resign-agent.rs
+++ src/bin/resign-agent.rs
@@ -1,12 +1,24 @@
 use anyhow::anyhow;
+use openpgp::crypto::Signer as SequoiaSigner;
+use openpgp::packet::key::Key4;
+use openpgp::packet::key::PublicParts;
+use openpgp::packet::key::UnspecifiedRole;
+use openpgp::packet::Key;
+use openpgp::serialize::MarshalInto;
+use openpgp::types::HashAlgorithm;
+use openpgp_card::OpenPgp;
+use openpgp_card_sequoia::card::Open;
 use pinentry::PassphraseInput;
 use secrecy::ExposeSecret;
 use secrecy::SecretString;
+use sequoia::PublicResponse;
+use sequoia_openpgp as openpgp;
 use ssh_agent_lib::proto::Blob;
 use ssh_agent_lib::proto::Signature;
 use std::collections::HashMap;
 use std::sync::Arc;
 use std::sync::Mutex;
+use std::time::SystemTime;
 
 use openpgp_card::algorithm::{Algo, Curve};
 use openpgp_card::crypto_data::PublicKeyMaterial;
@@ -101,6 +113,46 @@ impl SshAgentImpl {
             card.pin = None;
         }
     }
+    fn public(card: &mut OpenPgp) -> anyhow::Result<Key<PublicParts, UnspecifiedRole>> {
+        let tx = card.transaction()?;
+        let mut open = Open::new(tx)?;
+        let ctime = open.key_generation_times()?;
+        let ctime: SystemTime = ctime
+            .signature()
+            .ok_or(anyhow!("ctime for signature subkey ununavailable"))?
+            .to_datetime()
+            .into();
+        let key = open.public_key(KeyType::Signing)?;
+        let key: Key<PublicParts, UnspecifiedRole> = match key {
+            PublicKeyMaterial::E(k) => match k.algo() {
+                Algo::Ecc(attrs) => match attrs.curve() {
+                    Curve::Ed25519 => Key::V4(Key4::import_public_ed25519(k.data(), ctime)?),
+                    _ => unimplemented!(),
+                },
+                _ => unimplemented!(),
+            },
+            PublicKeyMaterial::R(k) => Key::V4(Key4::import_public_rsa(k.v(), k.n(), ctime)?),
+            _ => unimplemented!(),
+        };
+        Ok(key)
+    }
+    fn sign<'a>(
+        card: &mut OpenPgp,
+        hash_algo: HashAlgorithm,
+        digest: &[u8],
+        touch_prompt: &'a (dyn Fn() + Send + Sync),
+    ) -> anyhow::Result<openpgp::crypto::mpi::Signature> {
+        let key = Self::public(card)?;
+        let tx = card.transaction()?;
+        let mut open = Open::new(tx)?;
+        let mut sign = open
+            .signing_card()
+            .ok_or(anyhow!("failed to open signing card"))?;
+        let mut signer = sign.signer_from_pubkey(key, touch_prompt);
+        Ok(signer
+            .sign(hash_algo, digest)
+            .map_err(|e| anyhow!("signing failed: {}", e))?)
+    }
 }
 
 #[tonic::async_trait]
@@ -109,13 +161,54 @@ impl sequoia::signer_server::Signer for SshAgentImpl {
         &self,
         _request: tonic::Request<()>,
     ) -> Result<Response<sequoia::PublicResponse>, Status> {
-        Err(Status::unimplemented(""))
+        let inner = self.inner.lock().unwrap();
+        let ident = inner
+            .cards
+            .iter()
+            .next()
+            .ok_or(Status::unavailable("no card found"))?
+            .0
+            .clone();
+        drop(inner);
+        let mut card = openpgp_card_pcsc::PcscBackend::open_by_ident(&ident, None)
+            .map_err(|e| Status::unavailable(e.to_string()))?;
+        let mut card = openpgp_card::OpenPgp::new(&mut card);
+        let key = Self::public(&mut card).map_err(|e| Status::unavailable(e.to_string()))?;
+        Ok(Response::new(PublicResponse {
+            key: key.to_vec().unwrap(),
+        }))
     }
     async fn sign(
         &self,
-        _request: tonic::Request<sequoia::SignRequest>,
+        request: tonic::Request<sequoia::SignRequest>,
     ) -> Result<Response<sequoia::SignResponse>, Status> {
-        Err(Status::unimplemented(""))
+        let request = request.into_inner();
+        let inner = self.inner.lock().unwrap();
+        let ident = inner
+            .cards
+            .iter()
+            .next()
+            .ok_or(Status::unavailable("no card found"))?
+            .0
+            .clone();
+        drop(inner);
+        let mut card = openpgp_card_pcsc::PcscBackend::open_by_ident(&ident, None)
+            .map_err(|e| Status::unavailable(e.to_string()))?;
+        let mut card = openpgp_card::OpenPgp::new(&mut card);
+        let hash_algo = match request.hash_algo() {
+            sequoia::HashAlgorithm::Md5 => openpgp::types::HashAlgorithm::MD5,
+            sequoia::HashAlgorithm::Sha1 => openpgp::types::HashAlgorithm::SHA1,
+            sequoia::HashAlgorithm::RipeMd => openpgp::types::HashAlgorithm::RipeMD,
+            sequoia::HashAlgorithm::Sha224 => openpgp::types::HashAlgorithm::SHA224,
+            sequoia::HashAlgorithm::Sha256 => openpgp::types::HashAlgorithm::SHA256,
+            sequoia::HashAlgorithm::Sha384 => openpgp::types::HashAlgorithm::SHA384,
+            sequoia::HashAlgorithm::Sha512 => openpgp::types::HashAlgorithm::SHA512,
+        };
+        let sig = Self::sign(&mut card, hash_algo, &request.digest, &|| {})
+            .map_err(|e| Status::unavailable(e.to_string()))?;
+        Ok(Response::new(sequoia::SignResponse {
+            signature: sig.to_vec().unwrap(),
+        }))
     }
     async fn acceptable_hashes(
         &self,
