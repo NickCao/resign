df11a8b1ab7b23ffba4d796aa81055e33cae5ffe
Nick Cao <nickcao@nichi.co>
2022-07-21 16:34:51 +0800

    merge ssh agent into grpc agent

--- src/bin/resign-agent.rs
+++ src/bin/resign-agent.rs
@@ -1,6 +1,13 @@
+use service_binding::Binding;
+use service_binding::Listener;
+use ssh::agent_client::AgentClient;
+use ssh_agent_lib::Agent as SAgent;
 use std::sync::Arc;
 use std::sync::Mutex;
-use tonic::{transport::Server, Response, Status};
+use tokio::io::AsyncReadExt;
+use tonic::transport::Channel;
+use tonic::transport::Endpoint;
+use tonic::{transport::Server, Request, Response, Status};
 
 use openpgp::packet::Packet;
 use openpgp::serialize::MarshalInto;
@@ -105,14 +112,106 @@ impl ssh::agent_server::Agent for Agent {
     }
 }
 
+struct SshAgent {
+    client: Mutex<AgentClient<Channel>>,
+}
+
+impl ssh_agent_lib::Agent for SshAgent {
+    type Error = anyhow::Error;
+    fn handle(
+        &self,
+        request: ssh_agent_lib::proto::Message,
+    ) -> Result<ssh_agent_lib::proto::Message, Self::Error> {
+        match request {
+            ssh_agent_lib::proto::Message::RequestIdentities => {
+                let identities = futures::executor::block_on(async {
+                    self.client
+                        .lock()
+                        .unwrap()
+                        .identities(Request::new(()))
+                        .await
+                        .unwrap()
+                });
+                Ok(ssh_agent_lib::proto::Message::IdentitiesAnswer(
+                    identities
+                        .into_inner()
+                        .identities
+                        .iter()
+                        .map(|v| ssh_agent_lib::proto::Identity {
+                            comment: String::from_utf8(v.comment.clone()).unwrap(),
+                            pubkey_blob: v.key_blob.clone(),
+                        })
+                        .collect(),
+                ))
+            }
+            ssh_agent_lib::proto::Message::SignRequest(request) => {
+                let signature = futures::executor::block_on(async {
+                    self.client
+                        .lock()
+                        .unwrap()
+                        .sign(Request::new(ssh::SignRequest {
+                            key_blob: request.pubkey_blob,
+                            flags: request.flags,
+                            data: request.data,
+                        }))
+                        .await
+                        .unwrap()
+                });
+                Ok(ssh_agent_lib::proto::Message::SignResponse(
+                    signature.into_inner().signature,
+                ))
+            }
+            _ => Ok(ssh_agent_lib::proto::Message::Failure),
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "127.0.0.1:50051".parse()?;
     let agent = Agent::default();
-    Server::builder()
-        .add_service(sequoia::signer_server::SignerServer::new(agent.clone()))
-        .add_service(ssh::agent_server::AgentServer::new(agent.clone()))
-        .serve(addr)
-        .await?;
+    let (client, server) = tokio::io::duplex(1024);
+    tokio::spawn(
+        Server::builder()
+            .add_service(ssh::agent_server::AgentServer::new(agent.clone()))
+            .serve_with_incoming(futures::stream::iter(vec![Ok::<_, std::io::Error>(server)])),
+    );
+    tokio::spawn(
+        Server::builder()
+            .add_service(sequoia::signer_server::SignerServer::new(agent.clone()))
+            .serve(addr),
+    );
+
+    let mut client = Some(client);
+    let channel = Endpoint::try_from("http://127.0.0.1:0")
+        .unwrap()
+        .connect_with_connector(tower::service_fn(move |_: tonic::transport::Uri| {
+            let client = client.take();
+            async move {
+                if let Some(client) = client {
+                    Ok(client)
+                } else {
+                    Err(std::io::Error::new(
+                        std::io::ErrorKind::Other,
+                        "Client already taken",
+                    ))
+                }
+            }
+        }))
+        .await
+        .unwrap();
+    let client = ssh::agent_client::AgentClient::new(channel);
+    let agent = SshAgent {
+        client: Mutex::new(client),
+    };
+    agent
+        .listen(
+            "unix:///tmp/test"
+                .parse::<Binding>()
+                .unwrap()
+                .try_into()
+                .unwrap(),
+        )
+        .unwrap();
     Ok(())
 }
