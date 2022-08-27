88c0b83995ae87516a7f20c6f5733dac9de189ce
Nick Cao <nickcao@nichi.co>
2022-08-27 16:00:32 +0800

    remove extension

--- src/agent.rs
+++ src/agent.rs
@@ -3,7 +3,6 @@ use std::sync::Arc;
 use std::sync::Mutex;
 
 use openpgp_card::OpenPgp;
-use sequoia_openpgp as openpgp;
 
 #[derive(Default, Clone)]
 pub struct Agent {
@@ -38,30 +37,6 @@ impl ssh_agent_lib::Agent for Agent {
                 let signature = backend.auth_ssh(tx, &request.data)?;
                 Ok(ssh_agent_lib::proto::Message::SignResponse(signature))
             }
-            ssh_agent_lib::proto::Message::Extension(ext) => match ext.extension_type.as_str() {
-                "decrypt@nichi.co" => {
-                    let mut backend = self.backend.lock().unwrap();
-                    let mut card = backend.open()?;
-                    let mut card = OpenPgp::new(&mut card);
-                    let tx = card.transaction()?;
-                    let key = backend.public(tx, openpgp_card::KeyType::Decryption)?;
-                    let tx = card.transaction()?;
-                    let ciphertext = openpgp::crypto::mpi::Ciphertext::parse(
-                        key.pk_algo(),
-                        &*ext.extension_contents.0,
-                    )?;
-                    let plaintext = backend.decrypt(tx, &ciphertext, None, &|| {})?;
-                    Ok(ssh_agent_lib::proto::Message::Extension(
-                        ssh_agent_lib::proto::Extension {
-                            extension_type: "decrypt@nichi.co".to_string(),
-                            extension_contents: ssh_agent_lib::proto::ExtensionContents(
-                                plaintext.to_vec(),
-                            ),
-                        },
-                    ))
-                }
-                _ => Ok(ssh_agent_lib::proto::Message::Failure),
-            },
             _ => Ok(ssh_agent_lib::proto::Message::Failure),
         }
     }
